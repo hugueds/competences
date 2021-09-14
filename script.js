053df185dc184c7bc9f59e8ed4bd982accdef750
hugueds <hugo-pdf@hotmail.com>
2021-09-14 20:42:54 -0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function getSkill(){
+let selectedSkill = {};
+let data = {};
+let selectedTab = 0;
 
-}
 
-function render() {
+window.onload = async function(e) {
 
+    await fetch('skills.json')
+        .then(response => response.json())
+        .then(x => Object.assign(data, x)) 
+
+    const params = window.location.search;
+    const search = new URLSearchParams(params);
+
+    // selecionar o objeto adequado baseado nos parametros
+    // renderizar o menu lateral baseado no arquivo menu.js
+
+    let g = search.get('group')
+    let sg = search.get('subgroup')
+
+    let selected = data
+    // verificar a tab selecionada
+
+    
 }
 
